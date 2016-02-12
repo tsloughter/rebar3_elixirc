ff76a58871b0a7ac4723e91cc04b0ea89f26c052
Tristan Sloughter <t@crashfast.com>
2016-02-11 19:54:02 -0600

    new attempt not requiring a .app.src but won't work on deps yet

--- src/rebar3_elixirc_prv.erl
+++ src/rebar3_elixirc_prv.erl
@@ -26,30 +26,48 @@ init(State) ->
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
-    Apps = case rebar_state:current_app(State) of
-               undefined ->
-                   rebar_state:project_apps(State);
-               AppInfo ->
-                   [AppInfo]
-           end,
+    application:ensure_all_started(mix),
 
-    application:ensure_all_started(elixir),
+    Dir = rebar_state:dir(State),
+    Opts = rebar_state:opts(State),
+    CompilerOpts = rebar_opts:get(Opts, ex_opts, #{}),
+    elixir_config:put(compiler_options, CompilerOpts),
 
-    [begin
-         Opts = rebar_app_info:opts(AppInfo),
 
-         CompilerOpts = rebar_opts:get(Opts, ex_opts, #{}),
-         elixir_config:put(compiler_options, CompilerOpts),
+    [{Module, _}] = 'Elixir.Code':load_file(list_to_binary(filename:join(Dir, "mix.exs"))),
+    Config = Module:project(),
+    Name = proplists:get_value(app, Config),
+    AppSrc = app_src_file(Config),
+    AppSrcFile = filename:join([Dir, "src", atom_to_list(Name)++".app.src"]),
+    Spec = io_lib:format("~p.\n", [AppSrc]),
+    filelib:ensure_dir(AppSrcFile),
+    file:write_file(AppSrcFile, iolist_to_binary(Spec), [raw]),
 
-         OutDir = list_to_binary(rebar_app_info:ebin_dir(AppInfo)),
-         SourceDir = filename:join(rebar_app_info:dir(AppInfo), "lib"),
-         FoundFiles = [list_to_binary(X) || X <- rebar_utils:find_files(SourceDir, ".*\\.ex\$")],
+    SourceDir = filename:join(Dir, "lib"),
+    FoundFiles = [list_to_binary(X) || X <- rebar_utils:find_files(SourceDir, ".*\\.ex\$")],
 
-         'Elixir.Kernel.ParallelCompiler':files_to_path(FoundFiles, OutDir)
-     end || AppInfo <- Apps],
+    filelib:ensure_dir(filename:join([Dir, "ebin", "file"])),
+    'Elixir.Kernel.ParallelCompiler':files_to_path(FoundFiles, list_to_binary(filename:join(Dir, "ebin"))),
+
+    {_, AppInfo} = rebar_app_discover:find_app(Dir, all),
+    rebar_otp_app:compile(State, AppInfo),
 
     {ok, State}.
 
 -spec format_error(any()) ->  iolist().
 format_error(Reason) ->
     io_lib:format("~p", [Reason]).
+
+app_src_file(Config) ->
+    Name = proplists:get_value(app, Config),
+    Desc = binary_to_list(proplists:get_value(description, Config, "")),
+    Vsn = binary_to_list(proplists:get_value(version, Config, <<"0.0.1">>)),
+    Deps = [D || {D, _, Opts} <- proplists:get_value(deps, Config, []),
+                 not lists:member([{optional,true}], Opts), not lists:keymember(only, 1, Opts)],
+    {application, Name,
+     [{description, Desc},
+      {vsn, Vsn},
+      {modules,[]},
+      {registered,[]},
+      {applications,[kernel,stdlib | Deps]},
+      {env,[]}]}.
